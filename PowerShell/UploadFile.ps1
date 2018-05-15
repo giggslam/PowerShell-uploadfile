ef3b6b93d3ed621635ecd41eb759126e1a89dc79
Ryan <ryan.lam@amox.hk>
2018-05-15 17:14:10 +0800

    init

---
+++ PowerShell/UploadFile.ps1
@@ -0,0 +1,17 @@
+﻿$WatinPath = 'C:\Temp\WatiN-2.1.0.1196\bin\net40\WatiN.Core.dll'	#path with downloaded assembly
+$watin     = [Reflection.Assembly]::LoadFrom( $WatinPath )
+
+$file2upload = "C:\temp\File2Upload.jpg"
+
+#Set the URL we want to navigate to
+$url = "http://www.chatbot.hk/System/fileupload/upload.html" 
+
+$ie = new-object WatiN.Core.IE($url)
+$file1 = $ie.FileUpload('fileToUpload')	#id of the input
+$file1.set($file2upload)				# path to the file
+
+# and now just find the button and click on it
+#$o = $ie.Button | ? { $_.GetAttributeValue("name") -eq 'submit' }
+$o = $ie.Button('submit') #send is id of the submit button
+
+$o.Click()
---
+++ README.md
@@ -0,0 +1,13 @@
+# PowerShell Script for upload file to web
+
+2018-05-15
+
+## PowerShell
+
+* PowerShell 操控 I.E. 的 File browse 及 submnit
+* 因 I.E.有 security concern, 用 [WatiN](https://sourceforge.net/projects/watin/) 間接操控 I.E.
+
+
+## Web (php)
+
+* 確認收到 file field "fileToUpload" 後, 存檔於 "uploads" sub-folder
